130a8ea6a8200f81509b036338166ba0c70f7e7f
Lillevik <mariusfalch@hotmail.com>
2017-09-29 11:23:30 +0200

    Changed project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 app.config.from_object("config")
 CORS(app)
 
-import views
-
 if not os.path.isdir("uploads"):
     os.makedirs("uploads")
 
+from . import views
