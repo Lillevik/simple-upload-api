4d04e5ebb7c2abe8adc3b635762c5dd727cede34
Lillevik <mariusfalch@hotmail.com>
2017-10-04 18:43:01 +0200

    Support for max file size

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
 DOMAIN = "http://localhost:5000/"  # The last slash is important for now
-UPLOADS_FOLDER = os.getcwd() + "/uploads"
+UPLOADS_FOLDER = os.path.dirname(__file__) + "/uploads"
+MAX_CONTENT_LENGTH = 20 * 1024 * 1024 # Limit to 20 megabytes
