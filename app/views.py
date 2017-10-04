d2bce77034c3e362d1738a5e4909525f012404f2
Lillevik <mariusfalch@hotmail.com>
2017-10-04 18:57:44 +0200

    Removed hello world ;

--- app/views.py
+++ app/views.py
@@ -6,11 +6,6 @@ from flask import jsonify, request, send_file, abort
 from functions import generate_filename
 
 
-@app.route("/")
-def hellow_world():
-    return "Hello world!"
-
-
 @app.route('/upload/file', methods=["POST"])
 def general_file_upload():
     """ Uploads a file and saves it to the uploads folder declared in the config """
@@ -19,7 +14,7 @@ def general_file_upload():
         try:
             f = request.files['file']
             filename = generate_filename(f.filename)
-            file_path = os.path.join(app.config["UPLOADS_FOLDER"], filename)
+            file_path = os.path.join(app.config['UPLOADS_FOLDER'], filename)
             name_not_unique = True
             while name_not_unique:
                 if not os.path.isfile(file_path):
@@ -27,15 +22,14 @@ def general_file_upload():
                     f.save(file_path)
                 else:
                     filename = generate_filename(f.filename)
-            return jsonify({'success': {"status": 200,
+            return jsonify({'success': {'status': 200,
                                         'filename': filename,
-                                        'full_link': app.config["DOMAIN"] + filename}})
+                                        'full_link': app.config['DOMAIN'] + filename}})
         except RequestEntityTooLarge:
             abort(413)
         except Exception as e:
             print(e)
-            return jsonify({'error': 'Error uploading file.',
-                            'status': 413})
+            return jsonify({'error': 'Error uploading file.'})
     else:
         abort(405)
 
@@ -57,10 +51,10 @@ def page_not_found(e):
 
 @app.errorhandler(405)
 def method_not_allowed(e):
-    return 'Method not allowed', 405
+    return jsonify({'error': 'Method not allowed'}), 405
 
 
 @app.errorhandler(413)
 def method_not_allowed(e):
-    return jsonify({'error': 'File content too large. Max 20mb data.',
+    return jsonify({'error': 'File content too large. Max ' + app.config['MAX_CONTENT_LENGTH'] + 'mb data.',
                     'status': 413}), 413
